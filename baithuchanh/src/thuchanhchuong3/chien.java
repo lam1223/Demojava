29fdd80c8f2f6cb237882da000611a4e21401017
lam1223 <lamtung032@gmail.com>
2024-04-10 11:04:15 +0700

    4-10

---
+++ baithuchanh/src/thuchanhchuong3/chien.java
@@ -0,0 +1,40 @@
+package thuchanhchuong3;
+
+public class chien {
+    
+}
+public static void main(String[] args) {
+    hinhtron ht = new hinhtron();
+    ht.inTen();
+    ht.nhapBanKinh();
+    ht.tinhChuVi();
+    ht.tinhDienTich();
+    ht.inChuVi();
+    ht.inDienTich();
+
+    hinhchunhat hcn = new hinhchunhat();
+    hcn.inTen();
+    hcn.nhapChieuDai();
+    hcn.nhapChieuRong();
+    hcn.tinhChuVi();
+    hcn.tinhDienTich();
+    hcn.inChuVi();
+    hcn.inDienTich();
+
+    hinhtru htru = new hinhtru();
+    htru.inTen();
+    htru.nhapChieuCao();
+    htru.tinhTheTinh();
+    htru.inTheTich();
+
+    hinhvuong hv = new hinhvuong();
+    hv.inTen();
+    hv.nhapCanh();
+    hv.tinhChuVi();
+    hv.tinhDienTich();
+    hv.inChuVi();
+    hv.inDienTich();
+}
+
+
+}
